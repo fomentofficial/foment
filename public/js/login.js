985b51a77972c915feb9c31cc51e1bbd3f5c686d
seokhkang-k <fomentofficial@gmail.com>
2023-04-19 19:30:44 +0900

    4/19

--- public/js/login.js
+++ public/js/login.js
@@ -1,32 +1,38 @@
 // 네이버 로그인 관련 정리
 document.addEventListener('DOMContentLoaded', () => {
-  const naverLogin = new naver.LoginWithNaverId({
-    clientId: '<%= clientId %>',
-    callbackUrl: "/",
-    isPopup: false, // 팝업을 통한 연동처리 여부
-    callbackHandle: true
-  });
-
-  naverLogin.init(); // 이 부분을 수정해주세요.
 
   const btn = document.querySelector("#LogoutBtn");
+  const emailinfo = document.querySelector('.LoginInfo');
+  const LoginBound = document.querySelector('.LoginBound');
   const naverAccessToken = sessionStorage.getItem("naver_access_token");
   const naverEmail = sessionStorage.getItem("naver_email");
 
+  let naverId = "";
+  
+  if (naverEmail) {
+    const naverEmailInfo = naverEmail.split("@naver.com");
+    naverId = naverEmailInfo.slice(0, -1).join("");
+  }
+  
   console.log(naverAccessToken);
-  console.log(naverEmail);
-
+  console.log(naverId);
+  
   if (naverAccessToken) {
+    emailinfo.innerText = naverId;
     btn.innerText = "로그아웃";
+    LoginBound.style.display = "block";
     btn.addEventListener("click", () => {
       sessionStorage.removeItem("naver_access_token");
       sessionStorage.removeItem("naver_email"); // 이메일 정보도 삭제
       location.replace("/");
     });
   } else {
+    emailinfo.innerText = "";
     btn.innerText = "로그인";
+    LoginBound.style.display = "none";
     btn.addEventListener("click", () => {
       location.replace("/api_NaverLogin");
     });
   }
+  
 });
