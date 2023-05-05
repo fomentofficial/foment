d959f06324963fb9197b1cfce538549a8ee4f3c5
seokhkang-k <fomentofficial@gmail.com>
2023-05-05 12:31:28 +0900

    5/5

--- controllers/Ctrl_EditInvitation.js
+++ controllers/Ctrl_EditInvitation.js
@@ -1,20 +1,38 @@
+  // 데이터베이스 설정 가져오기
+  let dbConfig = require('../dbConfig');
+  let connection = dbConfig.connection
+  const path = require('path');
+  const fs = require('fs').promises;;
+
 // controllers/Ctrl_EditInvitation.js 파일
-function renderEditPage(req, res) {
+const renderEditPage = (req, res) => {
   const path = require('path');
   const fs = require('fs').promises;
+  const mysql = require('mysql');
 
-  const inviteURL = req.params.EditURLInfo; // :EditURLInfo 매개변수를 가져옴
+  // 클라이언트로부터[수정버튼] 전달받은 inviteURL정보
+  const inviteURL = req.params.EditURLInfo;
 
-  const TemplateFileName = `template_${inviteURL}.ejs`;
-  const TemplateFilePath = path.join(__dirname, '..', 'public', 'data', TemplateFileName);
+  // MySQL 데이터베이스에서 inviteURL과 일치하는 데이터 조회
+  connection.query('SELECT * FROM template WHERE template_ID = ?', inviteURL, (error, results, fields) => {
+    if (error) throw error;
 
-  const detaildata = {
-    pageTitle: 'Edit Invitation',
-    message: 'Please edit the invitation details'
-  };
-  res.render(TemplateFilePath, detaildata || {});
-}
+    // 조회한 결과를 HTML에 렌더링
+    const TemplateFileName = `template_${inviteURL}.ejs`;
+    const TemplateFilePath = path.join(__dirname, '..', 'public', 'data', TemplateFileName);
+
+    const detaildata = {
+      pageTitle: 'Edit Invitation',
+      message: 'Please edit the invitation details',
+      // MySQL에서 조회한 데이터를 전달
+      data: results[0]
+    };
+
+    
+    res.render(TemplateFilePath, detaildata || {});
+  });
+};
 
 module.exports = {
-  renderEditPage,
+  renderEditPage
 };
