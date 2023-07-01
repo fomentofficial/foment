635eeb1a9211340cd10fe50d5694f770c3af8073
seokhkang-k <fomentofficial@gmail.com>
2023-07-02 01:59:17 +0900

    7/2

--- controllers/Ctrl_AccountInfo.js
+++ controllers/Ctrl_AccountInfo.js
@@ -8,15 +8,15 @@ const accountInfoController = {
   postInfo: (req, res) => {
     let templateURL = req.params.GetURLInfo; // :EditURLInfo 매개변수 가져오기
     console.log(templateURL);
-
+  
     try {
       const dataArray = req.body;
-
+  
       // 데이터 유효성 검사
       if (!Array.isArray(dataArray)) {
         throw new Error('잘못된 데이터입니다.');
       }
-
+  
       // MySQL에서 template_ID와 동일한 user_ID 찾기
       const getUserIDQuery = 'SELECT user_ID FROM template WHERE template_ID = ?';
       connection.query(getUserIDQuery, [templateURL], (error, results) => {
@@ -25,48 +25,61 @@ const accountInfoController = {
           res.status(500).json({ error: '서버 오류' });
           return;
         }
-
-        // 찾은 user_ID와 함께 account 데이터 삽입
+  
+        // 찾은 user_ID와 함께 account 데이터 삽입 또는 업데이트
         const userID = results[0]?.user_ID;
         if (!userID) {
           res.status(404).json({ error: '해당 템플릿을 사용하는 사용자를 찾을 수 없습니다.' });
           return;
         }
-
+  
         // 각 계좌 그룹에 대한 처리
         for (const group of dataArray) {
           const { accordionTitle, innerData } = group;
-
+  
           // 내부 데이터 배열 처리
           if (!Array.isArray(innerData)) {
             throw new Error('잘못된 데이터입니다.');
           }
-
+  
           // 각 내부 데이터에 대한 처리
           for (const inner of innerData) {
             const { bankNameTerms, holderInfo, bankBankName, bankAccountInfo } = inner;
-
-            // 데이터를 사용하여 원하는 작업 수행
-            console.log(`계좌그룹: ${accordionTitle}`);
-            console.log(`은행명: ${bankNameTerms}`);
-            console.log(`예금주: ${holderInfo}`);
-            console.log(`은행명: ${bankBankName}`);
-            console.log(`계좌번호: ${bankAccountInfo}`);
-            console.log('---');
-
-            // MySQL에 데이터 삽입
-            const insertQuery = 'INSERT INTO account (template_ID, user_ID, accordionTitle, bankNameTerms, holderInfo, bankBankName, bankAccountInfo) VALUES (?, ?, ?, ?, ?, ?, ?)';
-            const insertValues = [templateURL, userID, accordionTitle, bankNameTerms, holderInfo, bankBankName, bankAccountInfo];
-            connection.query(insertQuery, insertValues, (error, results) => {
+  
+            // MySQL에서 template_ID와 accordionTitle이 일치하는 데이터 조회
+            const selectQuery = 'SELECT * FROM account WHERE template_ID = ? AND accordionTitle = ?';
+            const selectValues = [templateURL, accordionTitle];
+            connection.query(selectQuery, selectValues, (error, results) => {
               if (error) {
                 console.error(error);
               } else {
-                console.log('데이터가 성공적으로 삽입되었습니다.');
+                // 조회 결과에 따라 INSERT 또는 UPDATE 수행
+                if (results.length > 0) {
+                  const updateQuery = 'UPDATE account SET bankNameTerms = ?, holderInfo = ?, bankBankName = ?, bankAccountInfo = ? WHERE template_ID = ? AND accordionTitle = ?';
+                  const updateValues = [bankNameTerms, holderInfo, bankBankName, bankAccountInfo, templateURL, accordionTitle];
+                  connection.query(updateQuery, updateValues, (error, results) => {
+                    if (error) {
+                      console.error(error);
+                    } else {
+                      console.log('데이터가 성공적으로 업데이트되었습니다.');
+                    }
+                  });
+                } else {
+                  const insertQuery = 'INSERT INTO account (template_ID, user_ID, accordionTitle, bankNameTerms, holderInfo, bankBankName, bankAccountInfo) VALUES (?, ?, ?, ?, ?, ?, ?)';
+                  const insertValues = [templateURL, userID, accordionTitle, bankNameTerms, holderInfo, bankBankName, bankAccountInfo];
+                  connection.query(insertQuery, insertValues, (error, results) => {
+                    if (error) {
+                      console.error(error);
+                    } else {
+                      console.log('데이터가 성공적으로 삽입되었습니다.');
+                    }
+                  });
+                }
               }
             });
           }
         }
-
+  
         // 데이터를 성공적으로 처리한 후 클라이언트에 응답합니다.
         res.status(200).json({ message: '데이터가 성공적으로 처리되었습니다.' });
       });
--- controllers/Ctrl_CreateTemplate.js
+++ controllers/Ctrl_CreateTemplate.js
@@ -11,173 +11,199 @@ const short = require('short-uuid');
 
 // CreateTemplate 객체
 let CreateTemplate = {
+
   postInfo: (req, res) => {
     // 헤더에서 네이버 이메일 정보 가져오기
     const naver_email = req.headers['naver_email'];
-
+  
     console.log(naver_email);
-
+  
     // 사용자 정보 가져오기 쿼리
     const findUser = 'SELECT * FROM users WHERE naver_email=?';
     // 템플릿 생성 쿼리
-    const insertTemplate = 'INSERT INTO template (user_ID, template_ID, create_date, update_date) VALUES (?, ?, NOW(), NOW() )';
-
+    const insertTemplate = 'INSERT INTO template (user_ID, template_ID, create_date, update_date) VALUES (?, ?, NOW(), NOW())';
+    // account 생성 쿼리
+    const insertAccount = 'INSERT INTO account (user_ID, template_ID, accordionTitle, bankNameTerms, holderInfo, bankBankName, bankAccountInfo) VALUES (?, ?, ?, ?, ?, ?, ?)';
+  
     // 네이버 이메일로 사용자 정보 가져오기
     connection.query(findUser, [naver_email], (error, results, fields) => {
       if (error) {
         console.error(error);
-        res.status(500).send('Server Error');
-        return;
-      }
+        res.status(500).send('서버 에러');
+      } else if (results.length === 0) {
+        res.status(404).send('사용자를 찾을 수 없음');
+      } else {
+        // 사용자 ID 가져오기
+        const user_ID = results[0].ID;
+        // short-uuid 라이브러리로 새로운 템플릿 ID 생성하기
+        const su = short();
+        const template_ID = su.new();
+  
+        // 템플릿 생성하기
+        connection.query(insertTemplate, [user_ID, template_ID], (error, results, fields) => {
+          if (error) {
+            console.error(error);
+            res.status(500).send('서버 에러');
+          } else {
+            // 템플릿 파일 경로 및 파일명 정의
+            const templateFileName = 'detail.ejs';
+            const newTemplateFileName = `template_${template_ID}.ejs`;
+            const templateFilePath = path.join(__dirname, '..', 'public', 'views', templateFileName);
+            const newTemplateFilePath = path.join(__dirname, '..', 'public', 'data', newTemplateFileName);
 
-      if (results.length === 0) {
-        res.status(404).send('Not Found');
-        return;
-      }
-
-      // 사용자 ID 가져오기
-      const user_ID = results[0].ID;
-      // short-uuid 라이브러리로 새로운 템플릿 ID 생성하기
-      const su = short();
-      const template_ID = su.new();
-
-      // 템플릿 생성하기
-      connection.query(insertTemplate, [user_ID, template_ID], (error, results, fields) => {
-        if (error) {
-          console.error(error);
-          res.status(500).send('Server Error');
-          return;
-        }
-
-        // 템플릿 파일 경로 및 파일명 정의
-        const templateFileName = 'detail.ejs';
-        const newTemplateFileName = `template_${template_ID}.ejs`;
-        const templateFilePath = path.join(__dirname, '..', 'public', 'views', templateFileName);
-        const newTemplateFilePath = path.join(__dirname, '..', 'public', 'data', newTemplateFileName);
-
-        const Templatedata = {
-          Templatedata: {
-            data: {
-              theme_type: '',
-              BGM_type: 'NoneAudio',
-              effect_type: 'CherryblossomEffect',
-              font_type: 'GowunDodum-Regular',
-              font_size: 14,
-              invitation_title: '',
-              title_upload_img: '/Resource/mainasset/WeddingTitleImg.jpg',
-              kakao_share_img: '/Resource/mainasset/WeddingTitleImg.jpg',
-              groom_first_name: '',
-              groom_last_name: '',
-              select_groom_relationship: '아들',
-              groom_father_firstname: '',
-              groom_father_lastname: '',
-              groom_father_status: 0,
-              groom_father_status_type: '故',
-              groom_mother_firstname: '',
-              groom_mother_lastname: '',
-              groom_mother_status: 0,
-              groom_mother_status_type: '故',
-              bride_firstname: '',
-              bride_lastname: '',
-              select_bride_relationship: '딸',
-              bride_father_firstname: '',
-              bride_father_lastname: '',
-              bride_father_status: 0,
-              bride_father_status_type: '故',
-              bride_mother_firstname: '',
-              bride_mother_lastname: '',
-              bride_mother_status: 0,
-              bride_mother_status_type: '故',
-              wedding_date: '',
-              dday_toggle: 1,
-              wedding_AMPM: '오후',
-              wedding_time: '1시',
-              wedding_minute: '00분',
-              wedding_location: '',
-              wedding_location_hall: '',
-              wedding_address: '',
-              invite_title: '',
-              invite_body: '',
-              gallery_type: 'BoardType',
-              img_group_element: '[]',
-              board_password: '',
-              order_tab: '[{"class":"InviteArea","data-id":"Toggle_Invite"},{"class":"sec_cal is-visible","data-id":"Toggle_Calendar"},{"class":"GalleryArea","data-id":"Toggle_Gallery"},{"class":"LocationSection","data-id":"Toggle_WeddingLocation"},{"class":"AccountSection","data-id":"Toggle_Account"},{"class":"BoardSection","data-id":"Toggle_Board"}]',
-              accountData: [
-                {
-                  accordionTitle: '신랑측 계좌번호',
-                  innerData: [
+            const Templatedata = {
+              Templatedata: {
+                data: {
+                  theme_type: '',
+                  BGM_type: 'NoneAudio',
+                  effect_type: 'CherryblossomEffect',
+                  font_type: 'GowunDodum-Regular',
+                  font_size: 14,
+                  invitation_title: '',
+                  title_upload_img: '/Resource/mainasset/WeddingTitleImg.jpg',
+                  kakao_share_img: '/Resource/mainasset/WeddingTitleImg.jpg',
+                  groom_first_name: '',
+                  groom_last_name: '',
+                  select_groom_relationship: '아들',
+                  groom_father_firstname: '',
+                  groom_father_lastname: '',
+                  groom_father_status: 0,
+                  groom_father_status_type: '故',
+                  groom_mother_firstname: '',
+                  groom_mother_lastname: '',
+                  groom_mother_status: 0,
+                  groom_mother_status_type: '故',
+                  bride_firstname: '',
+                  bride_lastname: '',
+                  select_bride_relationship: '딸',
+                  bride_father_firstname: '',
+                  bride_father_lastname: '',
+                  bride_father_status: 0,
+                  bride_father_status_type: '故',
+                  bride_mother_firstname: '',
+                  bride_mother_lastname: '',
+                  bride_mother_status: 0,
+                  bride_mother_status_type: '故',
+                  wedding_date: '',
+                  dday_toggle: 1,
+                  wedding_AMPM: '오후',
+                  wedding_time: '1시',
+                  wedding_minute: '00분',
+                  wedding_location: '',
+                  wedding_location_hall: '',
+                  wedding_address: '',
+                  invite_title: '',
+                  invite_body: '',
+                  gallery_type: 'BoardType',
+                  img_group_element: '[]',
+                  board_password: '',
+                  order_tab: '[{"class":"InviteArea","data-id":"Toggle_Invite"},{"class":"sec_cal is-visible","data-id":"Toggle_Calendar"},{"class":"GalleryArea","data-id":"Toggle_Gallery"},{"class":"LocationSection","data-id":"Toggle_WeddingLocation"},{"class":"AccountSection","data-id":"Toggle_Account"},{"class":"BoardSection","data-id":"Toggle_Board"}]',
+                  accountData: [
                     {
-                      bankNameTerms: '신랑',
-                      holderInfo: '',
-                      bankBankName: '',
-                      bankAccountInfo: ''
+                      accordionTitle: '신랑측 계좌번호',
+                      innerData: [
+                        {
+                          bankNameTerms: '신랑',
+                          holderInfo: '',
+                          bankBankName: '',
+                          bankAccountInfo: ''
+                        },
+                        {
+                          bankNameTerms: '아버님',
+                          holderInfo: '',
+                          bankBankName: '',
+                          bankAccountInfo: ''
+                        },
+                        {
+                          bankNameTerms: '어머님',
+                          holderInfo: '',
+                          bankBankName: '',
+                          bankAccountInfo: ''
+                        }
+                      ]
                     },
                     {
-                      bankNameTerms: '아버님',
-                      holderInfo: '',
-                      bankBankName: '',
-                      bankAccountInfo: ''
-                    },
-                    {
-                      bankNameTerms: '어머님',
-                      holderInfo: '',
-                      bankBankName: '',
-                      bankAccountInfo: ''
-                    }
-                  ]
-                },
-                {
-                  accordionTitle: '신부측 계좌번호',
-                  innerData: [
-                    {
-                      bankNameTerms: '어머님',
-                      holderInfo: '',
-                      bankBankName: '',
-                      bankAccountInfo: ''
+                      accordionTitle: '신부측 계좌번호',
+                      innerData: [
+                        {
+                          bankNameTerms: '신부',
+                          holderInfo: '',
+                          bankBankName: '',
+                          bankAccountInfo: ''
+                        },
+                        {
+                          bankNameTerms: '아버님',
+                          holderInfo: '',
+                          bankBankName: '',
+                          bankAccountInfo: ''
+                        },
+                        {
+                          bankNameTerms: '어머님',
+                          holderInfo: '',
+                          bankBankName: '',
+                          bankAccountInfo: ''
+                        }
+                      ]
                     }
                   ]
                 }
-              ]
-            }
+              }
+            };
+  
+            // 템플릿 파일 렌더링
+            ejs.renderFile(templateFilePath, Templatedata, (err, html) => {
+              if (err) {
+                console.error(err);
+                res.status(500).send('서버 에러');
+              } else {
+                // 생성된 템플릿 파일 ID를 클라이언트로 전송
+                res.status(200).send(template_ID.toString());
+                console.log('새로운 템플릿 파일이 생성되었습니다: ' + newTemplateFileName);
+  
+                // account 데이터 추가
+                Templatedata.Templatedata.data.accountData.forEach((account) => {
+                  const { accordionTitle, innerData } = account;
+                  innerData.forEach((innerItem) => {
+                    const { bankNameTerms, holderInfo, bankBankName, bankAccountInfo } = innerItem;
+                    connection.query(insertAccount, [user_ID, template_ID, accordionTitle, bankNameTerms, holderInfo, bankBankName, bankAccountInfo], (error, results, fields) => {
+                      if (error) {
+                        console.error(error);
+                        res.status(500).send('서버 에러');
+                      } else {
+                        console.log('AccountData 전송 성공');
+                      }
+                    });
+                  });
+                });
+  
+                // 렌더링된 HTML 파일을 새 파일로 저장
+                fs.writeFile(newTemplateFilePath, html, (err) => {
+                  if (err) {
+                    console.error(err);
+                    res.status(500).send('서버 에러');
+                  }
+                });
+              }
+            });
           }
-        }
-        
-
-        // 템플릿 파일 렌더링, 여기 데이터를 ejs와 동일한 형식으로 변경
-        ejs.renderFile(templateFilePath, Templatedata, (err, html) => {
-          if (err) {
-            console.error(err);
-            res.status(500).send('Server Error');
-            return;
-          }
-          // 생성된 템플릿 파일 ID를 클라이언트로 전송
-          res.status(200).send(template_ID.toString());
-          console.log('새로운 템플릿 파일이 생성되었습니다: ' + newTemplateFileName);
-
-          // 렌더링된 HTML 파일을 새 파일로 저장
-          fs.writeFile(newTemplateFilePath, html, (err) => {
-            if (err) {
-              console.error(err);
-              res.status(500).send('Server Error');
-              return;
-            }
-          });
         });
-      });
+      }
     });
   },
 
+  
+  
   getInfo: async (req, res) => {
     try {
       const templateID = req.params.id;
       const templateFilePath = path.join(__dirname, '..', 'public', 'data', `template_${templateID}.ejs`);
-      
+
       // 템플릿 파일을 읽어서 내용을 가져옵니다.
       const templateFile = await fs.readFile(templateFilePath, 'utf-8');
-      
+
       // 템플릿 파일을 렌더링합니다.
       const renderedHtml = ejs.render(templateFile, { /* data */ });
-      
+
       // 렌더링된 HTML 코드를 응답으로 전송합니다.
       res.render(templateFilePath, { renderedHtml });
     } catch (err) {
@@ -185,8 +211,8 @@ let CreateTemplate = {
       res.status(500).send('Server Error');
     }
   }
-  
-  
+
+
 
 };
 
